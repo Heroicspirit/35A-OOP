40a500bfc31dc33995d376111e99e55019cd8b2d
Heroicspirit <astarana21@gmail.com>
2024-04-11 10:57:14 +0545

    Question 6 slove

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -37,5 +37,34 @@ instead of " !
 ***
 
 **** */
+        System.out.print("*********\n*********\n*********\n*********");
+        /*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
+        System.out.printin("A well-formed java program has a main method with { and } brances,\n A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
+        /*6. WAP that prints the following output:
+
+A well-formed Java program has a main method with { and } braces.
+
+
+
+A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+(But we type \" instead!)
+
+What is the difference between
+
+a ' and a "?  Or between a " and a \"?
+
+
+
+One is what we see when we're typing our program. The other is what appears on the "console." */
+
     }
 }
