5f94ab076ae1094dd1ba86b9bf51632734c7da0b
Heroicspirit <astarana21@gmail.com>
2024-04-11 10:20:43 +0545

    NUmber 2 question slove

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,5 +2,13 @@ public class Hello{
     public static void main(String[] args){
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World");
+        System.out.print("This is my first program\nI am on module ST4003CEM");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
     }
 }
