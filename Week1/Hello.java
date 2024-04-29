ac21001aef31865338518903d1556d5eaf50ef77
Heroicspirit <astarana21@gmail.com>
2024-04-29 07:33:02 +0545

    some error correct

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-    public static void main(String[] args){
+public static void main(String[] args){
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World");
         System.out.print("This is my first program\nI am on module ST4003CEM");
@@ -12,8 +12,6 @@ I am on module ST4003CEM
 System.out.print (not println) and produces the same output. */
 
         System.out.println("A \"quotaed\"String is\n'much' better if you can learn\nthe rules of \"escape sequences.\"\t\"is not the same\"");
-<<<<<<< HEAD
-=======
         /*3. Write a program that prints the following output:
 
 A "quoted" String is
@@ -49,7 +47,7 @@ instead of " !
 *********
 
 ********* */
-        System.out.printin("A well-formed java program has a main method with { and } brances,\n A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
+        System.out.println("A well-formed java program has a main method with { and } brances,\n A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?One is what we see when we're typing our program.\n The other is what appears on the \"console.\"");
         /*6. WAP that prints the following output:
 
 A well-formed Java program has a main method with { and } braces.
@@ -68,6 +66,5 @@ a ' and a "?  Or between a " and a \"?
 
 One is what we see when we're typing our program. The other is what appears on the "console." */
 
->>>>>>> 40a500bfc31dc33995d376111e99e55019cd8b2d
-    }
+        }
 }
